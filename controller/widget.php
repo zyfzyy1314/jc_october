f7a356c3daf6f408407fba473057a91b40ec4cf2
wangkekai <wkekai@163.com>
2019-09-17 17:07:28 +0800

    修改了查询的地址

--- controller/widget.php
+++ controller/widget.php
@@ -1,5 +1,5 @@
 <?php
-    $host = 'http://192.168.0.211:8110/metatrader/v1/marketdata';
+    $host = 'http://47.100.162.172:8089/metatrader/v1/marketdata';
 
     $params = [
         'symbols' => $_GET ['symbols']
@@ -12,7 +12,7 @@
     $header [] =  'Content-Type: application/json';
     array_push($header, 'accept: application/json');
 
-    array_push($header, 'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiJwaHBjbGllbnQiLCJqdGkiOiIzN2VhODA1OWVjOWI1MzNmYzAyYTRkYWJhMzlmN2M5NCIsInVuaXF1ZV9uYW1lIjoiNjY2NiIsIm5iZiI6MTU2NTA1OTEyMiwiZXhwIjoxNTk2NjgxNTIyLCJpYXQiOjE1NjUwNTkxMjIsImlzcyI6IkFnbU1ldGFUcmFkZXJBcGkiLCJhdWQiOiJBZ21NZXRhVHJhZGVyQXBpIn0.b86VGJNO7M8252ZS_1aQgvdLBMoNFlTc6z-R8Ek2wCdy8YOk6ee4B2t6INYKhTVh74yqt3Ih8ONy0eaogGwTX7qyN6ov7i2WDd1Hxjw8WUbvAwuOhanmUNhSuG5b1-YInpxjnTBZBM6Xgdw1cFb51-TOxrFqP8keJKGsDQGa3tZNIZIAcCEZ-HIh-fkXw13u2lKAJfa8LblaRohmURPeJL6KxufoiRr_EuFXjUnE6yUcLCDkOm83DcajYahJkEvS-m1qhpNlPir57u5iV8ELr1ZNy3JPPT8XsoVIV5aU6lLmOgTpl33oXxPFOOUbh8iNGr90xrGjLMTRwf3OhWHjrw');
+    array_push($header, 'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiJwaHBjbGllbnQiLCJqdGkiOiJiYWRhNGQzMzg3ZGZiOGVlNmQ5YTIwODQ5MDJhYmE2OSIsInVuaXF1ZV9uYW1lIjoiMTEwOSIsIm5iZiI6MTU2Mzc4NDc0MSwiZXhwIjoxODc5NDAzOTQxLCJpYXQiOjE1NjM3ODQ3NDEsImlzcyI6IkFnbU1ldGFUcmFkZXJBcGkiLCJhdWQiOiJBZ21NZXRhVHJhZGVyQXBpIn0.D94ApwoT-CM5QiXQU57_dX3vhedyYUsdjAWwrd38i3eWGLAUDoPBkYCEDt23rm0ovuwwjnBOA6KRS5LgvOiuJemwk9u1JAlRi241tiYFMfocLBM95KUxbWtSdz9sLvuRxxR3KPzRed-C14jlsGcaZpF9wRiQScv8kkrRiBgN51TdkLYNUGUWhADoOUYcCErXO5HvLFHBcLTkiW_74kSq3ze2dQzBwEEWo4pbNMrS2PrI2fRH5YdGrzqXF9P768oneb4JOVoUb30CD-DqapQxQsNPxT6iGen-pfuPiIXQBiEZHnhtTzkM4kTekE6zFmBd_BUocd9kLWHDKYqng5TFfA');
     
     $method = $_SERVER ['REQUEST_METHOD'];
     
@@ -47,7 +47,7 @@
     $data ['data'] = json_decode($outPut, true)['data'];
 
     if ($_GET ['type'] == 1) { // 查询历史开盘价
-        $host = 'http://192.168.0.211:8110/metatrader/v1/charts';
+        $host = 'http://47.100.162.172:8089/metatrader/v1/charts';
 
         $weekDay = date("w");
 
