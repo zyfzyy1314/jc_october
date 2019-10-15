c7b23a1f07cea413093d920b35df45d3a6146b6d
ghostwkk <kk.wang@miteke.com>
2019-10-15 16:20:03 +0800

    1、修改了样式

--- themes/demo/assets/js/economic_calendar_en.js
+++ themes/demo/assets/js/economic_calendar_en.js
@@ -350,6 +350,8 @@ new Vue({
 
                         // 使用刚指定的配置项和数据显示图表。
                         myChart.setOption(option);
+                    } else {
+                        $('#historyCharts').html('<p style="text-align: center;">No data</p>')
                     }
                 }
             })
